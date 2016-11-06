737f68be66e7ece75dfa9c0d748f900f0a2a1afe
Jacob Szemanski <jjsze13@gmail.com>
2016-11-06 14:12:40 -0600

    Added array of hashes problem

---
+++ adding_to_an_array_of_hashes.rb
@@ -0,0 +1,18 @@
+#create a loop that asks users for a first name, last name, and age  that gets added to an array of hashes
+
+people = []
+
+5.times do
+  person = {}
+puts "What is your first name?"
+person['first_name'] = gets.chomp
+
+puts "What is your last name?"
+person['last_name'] = gets.chomp
+
+puts "What is your age?"
+person['age'] = gets.chomp
+people << person
+end
+
+puts people
